722b421dbba2b6829a64eea42cc70485d07002eb
Ayndi <send_ayndi@mail.ru>
2022-03-14 12:09:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt('Ввм есть 18?', '18');
+// console.log(answer + 5);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// // const obj = {
+// //     a: 50 
+// // };
+
+// // obj.a = 10;
+
+// // console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ayndi';
+
+
+// const obj  = {
+//     name: 'Ayndi',
+//     age: 25,
+//     isMarried: true
+// };
+
+// // console.log(obj['name']);
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваше фвмилия?', '');
+// // answers[2] = prompt('Как вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+//Интерполяция
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
